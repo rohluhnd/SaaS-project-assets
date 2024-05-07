473455880b17946f727aa6cdc4a5cd4d78c61a58
Roland Ogbue <rolandajonathan@gmail.com>
2024-05-07 07:12:27 -0400

    updated javascript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,13 +11,13 @@ window.onscroll = function () {
 const navButton = document.querySelector("button[aria-expanded]");
 
 function toggleNav({ target }) {
-  const expanded = target.getAttribute("aria-expanded") === "true" || false;
+  const expanded = navButton.getAttribute("aria-expanded") === "true" || false;
   navButton.setAttribute("aria-expanded", !expanded);
 
   const headerNav = document.getElementById("topNav");
   if (!headerNav.classList.contains("sticky")) {
     headerNav.classList.add("sticky");
-  } else {
+  } else if (headerNav.classList.contains("sticky") && window.scrollY < 100) {
     headerNav.classList.remove("sticky");
   }
 }
